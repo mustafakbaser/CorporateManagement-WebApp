50be793ca4998de601b2def4d4b036e04a8f320c
MustafaBaser <32432625+MustafaBaser@users.noreply.github.com>
2021-01-29 18:15:05 +0300

    RakamIK Mikro Proje

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace RakamIKProjesi.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<RakamIKProjesi.Models.Siniflar.Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true; // güncellemeleri yansıtmak için true yapıyorum.
+        }
+
+        protected override void Seed(RakamIKProjesi.Models.Siniflar.Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Models/Siniflar/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace RakamIKProjesi.Models.Siniflar
+{
+    public class Context: DbContext
+    {
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Departmanlar> Departmans { get; set; }
+        public DbSet<Personel> Personels { get; set; }
+        public DbSet<Projeler> Projelers { get; set; }
+
+    }
+}
